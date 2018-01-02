c1363b239704a3489a54ace1126b460846b7c372
DIO <dhrubesh97@gmail.com>
2018-01-03 02:19:42 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -69,22 +69,6 @@ componentDidMount(){
   render() {
     return (
       <div className="App">
-	<div className="note">
-	<Label bsStyle="danger">
-	Note: {"\n"} I could only find one use case where this program {"\n"} would show up a wrong score.
-	When the first time the user selects a correct option, for some reason the component is not getting re-rendered. But the user 
-	clicks on the same right option for the 2nd time the component is getting re-rendered. Mind you this is only for the 1st
-	question. Now imagine a scenerio where the user clicks on a correct option for the first question and sees he's not re-directed
-	to the next question, then there's a high possibility of him clicking on the "Skip" button and hence even the right option's
-	score wouldn't be counted. Due to restriction in the time, I couldn't solve this. But excluding this if you could find any other
-	valid bugs feel free to tell me.
-	Also due to time restriction, I couldn't do the 4th (showcasing wrong choosen ans and correct ans of the same question).
-	If I had to do, I would have saved the question number and the wrong answers of them in a state(object) and later would have
-	printed them along with the right answers from the state(already existing) as I would have the question number in the previous
-	state.And if a write answer is selected after multiple wrong then I'll clear that part of the object.
- 	Thanks.
-	</Label>
-        </div>
 	<ShowQuestions
 	ques={this.state.questions}
 	listvalue={this.state.listvalue} 
